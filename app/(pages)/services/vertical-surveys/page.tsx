b284c961b293453e3774ddfe0136858cc2c398a2
leandrology <leandrology@gmail.com>
2023-08-12 01:24:07 +0800

    Transferred whole project to a new repo

---
+++ app/(pages)/services/vertical-surveys/page.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from 'next';
+
+import Image from 'next/image';
+import heroImg from '~/assets/images/hero.jpg';
+
+export const metadata: Metadata = {
+  title: `Vertical Surveys`,
+};
+
+const Page = () => {
+  return (
+    <>
+      <section className="bg-primary-50 dark:bg-slate-800">
+        <div className="mx-auto max-w-7xl px-4 pb-16 pt-1 text-center sm:px-6 md:pb-20 lg:px-8">
+          <h1 className="py-4 text-4xl md:text-5xl">Vertical Surveys</h1>
+          <div className="mx-auto max-w-7xl">
+            <div className="self-center md:basis-1/2">
+              <div className="ml-4">
+                {/* <h3 className="text-2xl font-medium leading-6 text-gray-900 dark:text-white">title here</h3> */}
+                <p className="mb-5 mt-2 text-lg text-gray-600 dark:text-slate-400">
+                  A vertical survey is a type of land survey that is used to measure the elevation of points on the
+                  ground relative to a known datum, such as sea level or a local benchmark. It is typically conducted by
+                  a licensed land surveyor and can be used for a variety of purposes
+                </p>
+              </div>
+              <div aria-hidden="true" className="mt-10 md:mt-0 md:basis-1/2">
+                <div className="relative m-auto max-w-4xl">
+                  <Image
+                    src={heroImg}
+                    width={100}
+                    height={500}
+                    alt="Hero alt"
+                    sizes="(max-width: 64rem) 100vw, 1024px"
+                    className="mx-auto h-auto w-full rounded-md bg-gray-400 dark:bg-slate-700"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;
---
+++ sanity/schemas/project.ts
@@ -0,0 +1,73 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Projects',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'Give a short and simple title',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      description: 'Just generate this once title has been provided',
+    }),
+    defineField({
+      name: 'subtitle',
+      title: 'Description',
+      type: 'string',
+      // of: [
+      //   {
+      //     type: 'block',
+      //   },
+      // ],
+      description: 'Give a description on your project',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      description: 'Choose an image of your project',
+    }),
+    // defineField({
+    //   name: 'content',
+    //   title: 'Content',
+    //   type: 'array',
+    //   of: [
+    //     {
+    //       type: 'block',
+    //     },
+    //     {
+    //       type: 'image',
+    //       fields: [
+    //         {
+    //           type: 'text',
+    //           name: 'alt',
+    //           title: 'Alternative Text',
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      subtitle: 'subtitle',
+      media: 'image',
+    },
+  },
+})
